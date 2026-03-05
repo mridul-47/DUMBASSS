df6a094102e597d76ff4125b95cb06bf6b279cac
mridul-47 <mridul.bennett@gmail.com>
2026-03-05 20:52:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function showPage(id){
+
+const pages = document.querySelectorAll(".page");
+
+pages.forEach(page=>{
+page.classList.remove("active");
+});
+
+document.getElementById(id).classList.add("active");
+
+}
+
+function openLetter(){
+
+setTimeout(()=>{
+showPage("letter");
+},400);
+
+}
